ee9b2ec1822f976e3720e223c350959b761fb8cc
Felipe V Ramos <fveigaramos@gmail.com>
2016-09-22 17:00:35 -0300

    +adicionado class principal +adicionado utils com informações de ip e portas +adicionada especificação do projeto

---
+++ src/br/edu/utfpr/cm/sd/chat/util/Utils.java
@@ -0,0 +1,8 @@
+package br.edu.utfpr.cm.sd.chat.util;
+
+public class Utils {
+    public static final String IP = "225.1.2.3";
+    public static final int PORTTOMULTICASTMESSAGES = 6789;
+    public static final int PORTTOPRIVATEMESSAGES = 6799;
+
+}
